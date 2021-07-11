33d42bf1053c2d0638210f1e986dbe8b6783b437
Matt <i.w.b.s.bilm@gmail.com>
2021-07-10 19:37:45 -0700

    added more content upload

--- oop-practice.js
+++ oop-practice.js
@@ -1,14 +1,48 @@
 //siple constructor
 
-function Car(make, model, price, power){
+function Car(make, model, year){
     this.make = make
     this.model = model
-    this.price = price
-    this.power = power
+    this.year = year
+    this.isRunning = false
+
+    // OR
+    // this.turnOn = function(){
+    //     this.isRunning = true
+    // }
+
+    // this.turnOff = function(){
+    //     this.isRunning = false
+    // }
+
+    // this.honk = function(){
+    //     if(this.isRunning)
+    //     return 'Beep'
+    // }
+   
 }
 
-var myCar = new Car('Toyota', 'Solaris', 24000, 700)
+Car.prototype.turnOn = function(){
+    this.isRunning = true
+}
+
+Car.prototype.turnOff = function(){
+    this.isRunning = false
+}
+
+Car.prototype.honk = function(){
+    if(this.isRunning)
+    return 'Beep'
+}
+
+
+var myCar = new Car('Toyota', 'Solaris', 2021)
 console.log(myCar);
+myCar.turnOn()
+console.log(myCar);
+// myCar.turnOff()
+console.log(myCar.honk());
+
 
 function Dog(name, age){
     this.name = name
@@ -21,7 +55,7 @@ function Dog(name, age){
 
 var myDog = new Dog ('Oopsy',4)
 
-myDog.bark()
+// myDog.bark()
 
 
 function Cat(){
@@ -36,9 +70,9 @@ function Cat(){
 }
 
 var myCat = new Cat('Tommy',4)
-
-console.log(myCat);
-myCat.bark()
+// Cat.prototype.name = 'Martin'
+// console.log(myCat);
+// myCat.bark()
 
 // PART 1
 
@@ -75,3 +109,136 @@ function Person(firstName,lastName,favoriteColor, favoriteNumber){
  function Child(firstName, lastName, favoriteColor, favoriteFood){
     Parent.apply(this, arguments)
  }
+
+
+
+
+ //------ Prototypes ------
+
+//  console.log(Parent.prototype);
+//  console.log(myCat.__proto__ === Cat.prototype);
+
+
+ var newData = []
+
+ newData = new Array
+//  console.dir(newData)
+ 
+
+ //-----
+ 
+ function User(name){
+     this.name = name
+ }
+
+ User.prototype.sayHi  = function(){
+     return 'Hi '+ this.name
+
+ }
+
+ var matt = new User('Matt')
+//  console.log(matt.sayHi())
+
+
+///----------------
+
+function Person(firstName, lastName, favoriteColor, favoriteNumber){
+    this.firstName = firstName 
+    this.lastName = lastName 
+    this.favoriteColor =  favoriteColor
+    this.favoriteNumber = favoriteNumber
+    this.family = []
+}
+
+
+Person.prototype.fullName = function(){
+    return this.firstName + ' ' + this.lastName
+}
+
+Person.prototype.addToFamily = function(newMember){
+    if(this.family.indexOf(newMember) === -1 && newMember instanceof Person){
+
+        this.family.push(newMember)
+    }
+    
+    
+    return this.family.length
+}
+
+
+
+Array.prototype.map = function(callback){
+
+    var newArray = []
+
+    for(var i=0; i < this.length; i++ ){
+
+        newArray.push(callback(this[i], i, this))
+    }
+
+    return newArray
+
+}
+
+String.prototype.reverse = function(){
+    var newStr = ''
+
+    for(var i = this.length -1; i >=0; i--){
+        newStr+= this[i]
+    }
+
+    return newStr
+}
+
+
+
+//Inheritance
+
+function Student(){
+    return Person.apply(this, arguments);
+}
+
+Student.prototype = Object.create(Person.prototype)
+Student.prototype.constructor = Student
+console.log(Student.prototype);
+var matt = Student('Matt', 'Hamilton','blue',13)
+
+console.log(Student.prototype);
+
+
+//----NEXT EXERCISE
+
+function Vehicle(make, model, year){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    
+}
+
+Vehicle.prototype.start = function(){
+    
+    return 'VROOM!';
+};
+
+
+Vehicle.prototype.toString = function(){
+    
+    return "The make, model, and year are "+ this.make + ' ' + this.model + ' ' + this.year  ;
+};
+
+
+function Car(make, model, year){
+    Vehicle.apply(this, arguments);
+    this.numWheels = 4;
+}
+
+Car.prototype = Object.create(Vehicle.prototype);
+Car.prototype.constructor = Car;
+
+function Motorcycle(make, model, year){
+    Vehicle.apply(this,arguments);
+    this.numWheels = 2;
+}
+
+Motorcycle.prototype = Object.create(Vehicle.prototype);
+Motorcycle.prototype.constructor = Motorcycle;
